0a024ba7d576130b216c1bdf0fb0f297b8ee1632
Nick Zalutskiy <pacemkr@gmail.com>
2017-02-15 02:47:32 -0500

    Refactor.

--- lib/link.ml
+++ lib/link.ml
@@ -6,39 +6,43 @@ module Lib = struct
 
   (* TODO: May not be such a great idea to use a record here?
    *       Since this one structure will be used to generate multiple rules. *)
+
+  type stubs = {
+    o_files: string list;
+    a_file: string;
+    dll_file: string;
+  }
+
+  type files = {
+    output: string;
+    cmo_files: string list;
+    cma_file: string;
+    stubs: stubs option;
+  }
+
   type ocamlmklib_opts = {
     pathL : string list;
     l : string list;
   }
 
-
-  type t = {
-    dir: string;
-    name: string;
-    deps: string list;
-    prods: string list;
-    ocamlmklib_opts : ocamlmklib_opts;
-  }
+  type t = files * ocamlmklib_opts
 
 
   let stdlib_path = Tools.run_ocamlfind_query "stdlib"
 
 
-  let add_ocamlmklib_l t clib =
-    let l = List.cons_uniq clib t.ocamlmklib_opts.l in
-    {t with ocamlmklib_opts = {t.ocamlmklib_opts with l}}
+  let add_ocamlmklib_l (files, opts) clib =
+    let l = List.cons_uniq clib opts.l in
+    (files, {opts with l})
 
 
-  let add_ocamlmklib_pathL t path =
+  let add_ocamlmklib_pathL ((files, opts) as t) path =
     if path = stdlib_path then t else
-    let pathL = List.cons_uniq path t.ocamlmklib_opts.pathL in
-    {t with ocamlmklib_opts = {t.ocamlmklib_opts with pathL}}
-
+    (files, {opts with pathL = List.cons_uniq path opts.pathL})
 
 
   let create ?o_files ~ml_files ~dir ~name =
     let dir = Filename.normalize (dirname dir) in
-
     (* Link order matters for libraries.
      * `.cmo` files need to be listed in dependency order for ocamlmklib
      * or an invalid library will be produced. *)
@@ -46,31 +50,27 @@ module Lib = struct
       Tools.run_ocamlfind_ocamldep_sort ml_files |>
       List.map ~f:(replace_suffix_exn ~old:".ml" ~new_:".cmo") in
 
-    let deps, prods = match o_files with
-      | Some o_files ->
-        let deps = o_files@cmo_files in
-        let prods = [
-          sprintf "%s/dll%s.so" dir name;
-          sprintf "%s/lib%s.a" dir name;
-          sprintf "%s/%s.cma" dir name;
-        ] in
-        (deps, prods)
+    let stubs = match o_files with
+      | Some o_files -> Some {
+          o_files;
+          a_file = sprintf "%s/lib%s.a" dir name;
+          dll_file = sprintf "%s/dll%s.so" dir name;
+        }
+      | None -> None in
 
-      | None ->
-        let prods = [
-          sprintf "%s/%s.cma" dir name;
-        ] in
-        (cmo_files, prods)
-    in
+    let files = {
+      cmo_files; stubs;
+      cma_file = sprintf "%s/%s.cma" dir name;
+      output = sprintf "%s/%s" dir name;
+    } in
 
-    {
-      dir; name;
-      deps; prods;
-      ocamlmklib_opts = {
-        pathL = [];
-        l = [];
-      }
-    }
+    let opts = {
+      pathL = [];
+      l = [];
+    } in
+
+    (files, opts)
+
 
 
   let link_packages t ~packages =
@@ -83,10 +83,25 @@ module Lib = struct
     List.fold_left ~f:add_ocamlmklib_l ~init:t clibs
 
 
-  let install_rules {dir; name; deps; prods; ocamlmklib_opts = {pathL; l} } =
-    let o = sprintf "%s/%s" dir name in
-    Rule.rule ~deps ~prods (fun _ _ ->
-        Tools.ocamlmklib ~o ~pathL ~l deps
-      )
+  (* TODO *)
+  (* - Build .so/.a, .cma, .cmxa in three separate steps. *)
+  (* - Pass cmx files. *)
+  (* - For "native plugins" .cmxs we may need to build using ocamlc directly. *)
+  (*   Or, install_rules_targeting_native_plugin which passes through extra arguments to ocamlopt. *1) *)
+  let install_rules ({output = o; stubs; cmo_files; cma_file}, {pathL; l}) =
+    match stubs with
+    | Some {o_files; a_file; dll_file} ->
+      (* Build .so and .a files. *)
+      let stub_libs = [a_file; dll_file] in
+      Rule.rule ~deps:o_files ~prods:stub_libs (fun _ _ ->
+          Tools.ocamlmklib ~o ~pathL ~l o_files
+        );
+
+      (* Build .cma file. *)
+      Rule.rule ~deps:(stub_libs@cmo_files) ~prods:[cma_file] (fun _ _ ->
+          Tools.ocamlmklib ~o ~pathL ~l cmo_files
+        );
+
+    | None -> ()
 
 end
